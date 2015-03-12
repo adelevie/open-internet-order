0ef80390a45fa502e159a928793ca8837a559153
Alan deLevie <adelevie@gmail.com>
2015-03-12 11:41:54 -0400

    Made spaces safe in search_terms

--- search.sh
+++ search.sh
@@ -1,6 +1,7 @@
 search_terms=( "find" "conclude" "agree" "disagree" "reject" )
 
-for i in "${search_terms[@]}"
+IFS=""
+for term in ${search_terms[*]}
 do
-  grep -R $i order.txt --no-filename --context --color=auto > output.txt
+    grep -R "${term}" order.txt --no-filename --context --color=auto > output.txt
 done
